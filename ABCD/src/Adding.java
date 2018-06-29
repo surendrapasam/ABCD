e669f95227df9ebb64bb7d58a566166e5b43290e
surendra <surendra@DESKTOP-PBO334S>
2018-06-29 13:26:43 +0530

    pushing my project

---
+++ ABCD/src/Adding.java
@@ -0,0 +1,13 @@
+
+public class Adding {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=12;
+		int b=22;
+		int c=a+b;
+		System.out.println(c);
+
+	}
+
+}
